76a0e4af646124dc8b649755f0c79388152fcf35
keilaloia <keil.aloia@students.aie.edu.au>
2017-09-28 16:44:58 -0700

    almost done octopi

--- Assets/scripts/ClassTako.cs
+++ Assets/scripts/ClassTako.cs
@@ -6,11 +6,16 @@ public class ClassTako : ClassBase {
 
     public float HitDistance = 1.2f;
     public Rigidbody2D rb;
+    float tempdt = 0f;
     RaycastHit2D[] hit;
+    float startTime;
+    float elapsedTime;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    public override void Start()
     {
+         startTime = Time.deltaTime;
+
         control.isJumping = false;
         control.maxJumpForce = 5f;
 
@@ -19,39 +24,52 @@ public class ClassTako : ClassBase {
     }
 	
 	// Update is called once per frame
-	void Update ()
+	public override void Update ()
     {
-
-        hit = Physics2D.RaycastAll(transform.position, -transform.up, HitDistance);
-        for (int i = 0; i < hit.Length; i++)    
+        if (!control.isEnemyAI)
         {
-            if (hit != null && hit[i] != hit[i].collider.gameObject.tag.Equals("Player")) 
+            hit = Physics2D.RaycastAll(transform.position, -transform.up, HitDistance);
+            for (int i = 0; i < hit.Length; i++)
             {
-                Debug.Log(hit[i].collider.gameObject.name);
-                control.isJumping = true;
+                if (hit != null && hit[i] != hit[i].collider.gameObject.tag.Equals("Player"))
+                {
+                    Debug.Log(hit[i].collider.gameObject.name);
+                    control.isGrounded = true;
+                   
+                    control.isJumping = false;
+
+                }
+                else if(control.isJumping == true)
+                {
+                    control.isGrounded = false;
+                }
             }
-            else
+            Debug.DrawRay(transform.position, -transform.up * HitDistance, Color.red);
+
+
+
+            //RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+            //if(hit.collider != null)
+            //{
+            //    float distance = Mathf.Abs(hit.point.y - transform.position.y);
+            //    float heightError = floatHeight - distance;
+            //    // float force = lifeForce * heightError - rb.velocity.y * damping
+            //    return hit;
+
+            //}
+            if (control.isGrounded == true && Input.GetKeyDown(KeyCode.Space))
             {
-                control.isJumping = false;
+                elapsedTime = Time.time - startTime;
+                Debug.Log("Charging...");
             }
-        }
-        Debug.DrawRay(transform.position, -transform.up * HitDistance, Color.red);
+            if(control.isGrounded == true && Input.GetKeyUp(KeyCode.Space))
+            {
+                    rb.AddForce(Vector2.up * control.maxJumpForce , ForceMode2D.Impulse);
+                    control.isJumping = true;
+                Debug.Log("JAAAUP...");
+            }
 
-
-
-        //RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
-        //if(hit.collider != null)
-        //{
-        //    float distance = Mathf.Abs(hit.point.y - transform.position.y);
-        //    float heightError = floatHeight - distance;
-        //    // float force = lifeForce * heightError - rb.velocity.y * damping
-        //    return hit;
-
-        //}
-        if (control.isJumping == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * control.maxJumpForce, ForceMode2D.Impulse);
- 
+            
         }
 	}
 }
