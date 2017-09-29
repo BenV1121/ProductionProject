57a591390d79c21ff4a167abc77468dd33ce7b10
Raphael Mastel III <s179135@students.aie.edu.au>
2017-09-28 17:55:54 -0700

    HEEEEYYYYY

---
+++ Assets/scripts/classBEnvironment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class classBEnvironment : MonoBehaviour {
+
+    
+    void breakMe()
+    {
+        Destroy(gameObject);   
+    }
+
+    void onCollisionEnter(Collision collider)
+    {
+        //New ClassBase() is a place holder, replace it with the Rock man class that aaron is working on.
+        if(collider.gameObject.GetComponent<BaseController>().playerClass == new ClassBase() &&
+           collider.gameObject.GetComponent<BaseController>().playerState == BaseController.PlayerState.ATTACK)
+        {
+            breakMe();
+        }
+    }
+}
