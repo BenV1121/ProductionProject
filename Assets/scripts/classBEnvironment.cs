f6b088a6f216b8dcdc66dd9a2bdcdb40f7d3d25c
Raphael Mastel III <s179135@students.aie.edu.au>
2017-09-29 10:51:05 -0700

    Finished destructable wood and rock walls

--- Assets/scripts/classBEnvironment.cs
+++ Assets/scripts/classBEnvironment.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 
 public class classBEnvironment : MonoBehaviour {
 
-    
+    //im using this to indicate wether a wall is wood or rock
+    public enum walltyp { ROCK, WOOD};
+    public walltyp thiswallis;
+
+    //destroys the object this class is attached to
     void breakMe()
     {
-        Destroy(gameObject);   
+        Destroy(gameObject);
     }
 
     void onCollisionEnter(Collision collider)
     {
+        //If the wall is rock and is attacked by a rock dude, destroy it
         //New ClassBase() is a place holder, replace it with the Rock man class that aaron is working on.
-        if(collider.gameObject.GetComponent<BaseController>().playerClass == new ClassBase() &&
+        if(thiswallis == walltyp.ROCK &&
+           collider.gameObject.GetComponent<BaseController>().playerClass == new ClassBase() &&
            collider.gameObject.GetComponent<BaseController>().playerState == BaseController.PlayerState.ATTACK)
+        {
+            breakMe();
+        }
+
+        //if the wall is wood and is hit by a fireball destroy it
+        if(thiswallis == walltyp.WOOD &&
+           collider.gameObject.tag == "FireBall")
         {
             breakMe();
         }
