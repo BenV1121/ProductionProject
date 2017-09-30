6c38adaff3b04806af5a1037715a3847b2cdf929
Benjamin Vongtawan <s179141@students.aie.edu.au>
2017-09-29 17:51:05 -0700

    spikes

---
+++ Assets/scripts/SpikeTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTimer : MonoBehaviour
+{
+    public float timer;
+    float refillTimer;
+
+    public float activateTimer = 0;
+    float refillActiveTimer = 5;
+
+    // Use this for initialization
+    void Start ()
+    {
+        ////timer = 5;
+        ////refillTimer = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            timer = refillTimer;
+
+            gameObject.SetActive(false);
+
+            activateTimer -= Time.deltaTime;
+        }
+
+        if (activateTimer <= 0)
+        {
+            activateTimer = refillActiveTimer;
+
+            gameObject.SetActive(true);
+        }
+	}
+}
