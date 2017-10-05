fd342c77d06d77bfd002682e9919f9d649ca628a
Aaron Andrews <s179120@students.aie.edu.au>
2017-10-05 10:24:58 -0700

    added punching anims

---
+++ Assets/scripts/Rock_Enemy_Punch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock_Enemy_Punch : MonoBehaviour {
+
+    GameObject parent;
+    enum STATE { MOVE_LEFT, MOVE_RIGHT, STAND_STILL, LANDING, REEL_BACK, ATTACK }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(parent.GetComponent<Rock_Enemy>().curr_state == STATE.ATTACK)
+        {
+
+        }
+    }
+}
