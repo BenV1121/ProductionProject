2c4d53f43d360e6d4669e7d37aaf259689b717ad
s179140 <s179140@SEA-8E3F0D>
2017-10-06 14:45:48 -0700

    blah

--- Assets/scripts/ParticleController.cs
+++ Assets/scripts/ParticleController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ParticleController : MonoBehaviour {
 
-    GameObject embers, jumpPoof, stone;
+    GameObject jumpPoof;
 
 	// Use this for initialization
 	void Start () {
