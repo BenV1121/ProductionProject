6901082457cce36ed7e936671929d64462e396a9
s179140 <s179140@SEA-8E3F0D>
2017-10-06 14:41:14 -0700

    start work on juice engine

---
+++ Assets/scripts/ParticleController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour {
+
+    GameObject embers, jumpPoof, stone;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
